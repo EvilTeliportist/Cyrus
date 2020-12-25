dcf34430b72a5b99c894407f0576c7d63e48709a
EvilTeliportist <brhoulton@gmail.com>
2020-12-25 15:35:33 -0500

    changed out files'

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import os, time, subprocess, playsound, random, datetime
+import os, time, subprocess, random, datetime, playsound
 import speech_recognition as sr
 from gtts import gTTS
 
 
 def get_audio(source):
+
+    return input("() :  ").lower()
+
+    '''
     r = sr.Recognizer()
 
     audio = r.listen(source)
@@ -16,6 +20,7 @@ def get_audio(source):
 
     print(said.lower())
     return said.lower()
+    '''
 
 def count(words, text):
     count = 0;
@@ -25,21 +30,24 @@ def count(words, text):
     return count;
 
 def speak(text):
+
     aud = gTTS(text=text, lang='en-uk', slow=False)
     aud.save("aud.mp3")
     playsound.playsound('aud.mp3', True)
     os.remove('aud.mp3')
 
-WAKES = ["iris"]
+
+WAKES = ["iris", "cyrus"]
 GREETINGS = ['How can I help you?', 'Sir.', 'Here, sir.', 'Nice to see you.', 'Good day, sir.']
 QUITS = ["quit", "exit"]
+SLEEP = ['sleep', 'thanks', 'thank you']
+FILLERS = ['um', 'uh']
 print("Start")
 running = True;
 woken = False;
 
 while running:
     with sr.Microphone() as source:
-        print("Listening")
         text = get_audio(source)
 
         if count(WAKES, text) > 0:
@@ -49,14 +57,20 @@ while running:
                 speak(random.choice(GREETINGS))
             woken = True
 
-        print("Text: " + text)
 
         # -------------- AlL WOKEN COMMANDS ---------------
         if woken:
 
             if count(["time"], text) > 0:
                 now = datetime.datetime.now()
-                time = now.strftime("%H hours and %M minutes")
+                h = int(now.strftime("%H"))
+                pm = 'AM'
+
+                if h > 12:
+                    pm = 'PM'
+                    h -= 12
+
+                time = now.strftime("%H:%M " + pm)
                 speak("It is currently " + str(time))
 
             if count(SLEEP, text) > 0:
